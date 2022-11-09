c35f9d00b0b7de38d17dd7e3bd8fe0c22afb4d4c
Alexis Brownstein <alexis@drdatabase.com>
2022-11-09 17:46:44 -0500

    did file and folder setup and schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(30) NOT NULL
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_id INT,
+  title VARCHAR(30) NOT NULL,
+  salary decimal not null,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  role_id INT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) not null,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
+
---
+++ index.js
@@ -0,0 +1,4 @@
+// Import and require mysql2
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const PORT = process.env.PORT || 3001;
